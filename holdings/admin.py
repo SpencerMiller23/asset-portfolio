d000172f2fdd6c8cfe83b362217d12449a18687c
Spencer Miller <spencer.miller@me.com>
2020-10-29 16:40:19 +0200

    Added necessary models and login functionality

--- holdings/admin.py
+++ holdings/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from holdings.models import User, Position, Profile
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Position)
+admin.site.register(Profile)
--- holdings/views.py
+++ holdings/views.py
@@ -1,15 +1,70 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.http import HttpResponse
 
 import requests
 import json
 
+from .models import User, Position, Profile
+
 # Create your views here.
 def index(request):
     return render (request, "holdings/layout.html")
 
-# Create your views here.
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("dashboard"))
+        else:
+            return render(request, "holdings/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "holdings/login.html")
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "holdings/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(request, "holdings/register.html", {
+                "message": "Username already taken."
+            })
+        login(request, user)
+        profile = Profile()
+        profile.user = user
+        profile.save()
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "holdings/register.html")
+
+@login_required
 def dashboard(request):
     response = requests.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY", params={"symbol": "AAPL", "apikey": "4JU2DZ6Q8876MFXK"})
     data = response.json()
