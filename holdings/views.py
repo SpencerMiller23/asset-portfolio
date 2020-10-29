e3d12ee254c3bb7d7a2a1312cfe3e14c8e6ffae6
Spencer Miller <spencer.miller@me.com>
2020-10-29 12:39:16 +0200

    sixth commit

--- holdings/views.py
+++ holdings/views.py
@@ -1,6 +1,25 @@
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render
 from django.http import HttpResponse
+
+import requests
+import json
 
 # Create your views here.
 def index(request):
     return render (request, "holdings/layout.html")
+
+# Create your views here.
+def dashboard(request):
+    response = requests.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY", params={"symbol": "AAPL", "apikey": "4JU2DZ6Q8876MFXK"})
+    data = response.json()
+    symbol = data["Meta Data"]["2. Symbol"]
+    price = data["Time Series (Daily)"]["2020-10-28"]["4. close"]
+    response = requests.get("https://www.alphavantage.co/query?function=OVERVIEW", params={"symbol": "AAPL", "apikey": "4JU2DZ6Q8876MFXK"})
+    data = response.json()
+    name = data["Name"]
+    return render (request, "holdings/dashboard.html", {
+        'symbol': symbol,
+        'name': name,
+        'price': price
+    })
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -81,6 +81,7 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = 'holdings.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
