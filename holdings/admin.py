0478ddbd40098083bba180be3cd14fb9ccae3bf1
removedporn <86824510+removedporn@users.noreply.github.com>
2023-07-16 21:20:22 +0800

    Delete holdings directory

--- holdings/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from holdings.models import User, Position, Profile
-
-# Register your models here.
-admin.site.register(User)
-admin.site.register(Position)
-admin.site.register(Profile)
